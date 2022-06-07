5313bbe1b264f52301e25e8178cf72b1c33faca7
Arwin-Yu <102544244+Arwin-Yu@users.noreply.github.com>
2022-06-07 16:39:37 +0800

    Update visualization.py

---
+++ utils/visualization.py
@@ -0,0 +1,93 @@
+import torch.nn as nn
+import torch 
+import torch 
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000, init_weights=False):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[48, 27, 27]
+            nn.Conv2d(48, 128, kernel_size=5, padding=2),           # output[128, 27, 27]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 13, 13]
+            nn.Conv2d(128, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.Conv2d(192, 128, kernel_size=3, padding=1),          # output[128, 13, 13]
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(128 * 6 * 6, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(2048, 2048),
+            nn.ReLU(inplace=True),
+            nn.Linear(2048, num_classes),
+        )
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        outputs = []
+        for name, module in self.features.named_children():
+            x = module(x)
+            if name in ["0", "3", "6"]:
+                outputs.append(x)
+
+        return outputs
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+
+data_transform = transforms.Compose(
+    [transforms.Resize((224, 224)),
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+ 
+# create model
+model = AlexNet(num_classes=5) 
+# load model weights
+model_weight_path = "./AlexNet.pth"   
+model.load_state_dict(torch.load(model_weight_path))
+print(model)
+
+# load image
+img = Image.open("../tulip.jpg")
+# [N, C, H, W]
+img = data_transform(img)
+# expand batch dimension
+img = torch.unsqueeze(img, dim=0)
+
+# forward
+out_put = model(img)
+for feature_map in out_put:
+    # [N, C, H, W] -> [C, H, W]
+    im = np.squeeze(feature_map.detach().numpy())
+    # [C, H, W] -> [H, W, C]
+    im = np.transpose(im, [1, 2, 0])
+
+    # show top 12 feature maps
+    plt.figure()
+    for i in range(12):
+        ax = plt.subplot(3, 4, i+1)
+        # [H, W, C]
+        plt.imshow(im[:, :, i], cmap='gray')
+    plt.show()
