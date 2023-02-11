63045a4f7b665af830aa9aca33b123e0b2746f93
Arwin-Yu <102544244+Arwin-Yu@users.noreply.github.com>
2023-02-11 22:44:41 +0000

    Update train.py

--- train.py
+++ train.py
@@ -2,14 +2,15 @@
 # 相较于简单版本的训练脚本 train_sample 增添了以下功能：
 # 1. 使用argparse类实现可以在训练的启动命令中指定超参数
 # 2. 可以通过在启动命令中指定 --seed 来固定网络的初始化方式，以达到结果可复现的效果
-# 3. 可以通过在启动命令中指定 --tensorboard 来进行tensorboard可视化, 默认不启用。
-# 4. 可以通过在启动命令中指定 --model 来选择使用的模型
+# 3. 使用了更高级的学习策略 cosine warm up：在训练的第一轮使用一个较小的lr（warm_up），从第二个epoch开始，随训练轮数逐渐减小lr。 
+# 4. 可以通过在启动命令中指定 --model 来选择使用的模型 
+# 5. 使用amp包实现半精度训练，在保证准确率的同时尽可能的减小训练成本
+# 6. 实现了数据加载类的自定义实现
+# 7. 可以通过在启动命令中指定 --tensorboard 来进行tensorboard可视化, 默认不启用。
 #    注意，使用tensorboad之前需要使用命令 "tensorboard --logdir= log_path"来启动，结果通过网页 http://localhost:6006/'查看可视化结果
-# 5. 使用了一个更合理的学习策略：在训练的第一轮使用一个较小的lr（warm_up），从第二个epoch开始，随训练轮数逐渐减小lr。
-# 6. 使用amp包实现半精度训练，在保证准确率的同时尽可能的减小训练成本
 ############################################################################################################
 # --model 可选的超参如下：
-# alexnet   zfnet   vgg   vgg_tiny   vgg_small   vgg_big   googlenet   resnet_small   resnet   resnet_big   resnext   resnext_big  
+# alexnet   zfnet   vgg   vgg_tiny   vgg_small   vgg_big   googlenet   xception   resnet_small   resnet   resnet_big   resnext   resnext_big  
 # densenet_tiny   densenet_small   densenet   densenet_big   mobilenet_v3   mobilenet_v3_large   shufflenet_small   shufflenet
 # efficient_v2_small   efficient_v2   efficient_v2_large   convnext_tiny   convnext_small   convnext   convnext_big   convnext_huge
 # vision_transformer_small   vision_transformer   vision_transformer_big   swin_transformer_tiny   swin_transformer_small   swin_transformer 
@@ -29,20 +30,20 @@ from torchvision import transforms
 import torch.optim.lr_scheduler as lr_scheduler 
 
 import classic_models 
-from utils.lr_methods import warmup
-from dataload.dataload_mini_imagenet import MyDataSet
+from utils.lr_methods import warmup 
 from dataload.dataload_five_flower import Five_Flowers_Load
 from utils.train_engin import train_one_epoch, evaluate 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_classes', type=int, default=100, help='the number of classes')
+parser.add_argument('--num_classes', type=int, default=5, help='the number of classes')
 parser.add_argument('--epochs', type=int, default=50, help='the number of training epoch')
-parser.add_argument('--batch_size', type=int, default=256, help='batch_size for training')
+parser.add_argument('--batch_size', type=int, default=64, help='batch_size for training')
 parser.add_argument('--lr', type=float, default=0.0002, help='star learning rate')
-parser.add_argument('--lrf', type=float, default=0.0001, help='end learning rate')
+parser.add_argument('--lrf', type=float, default=0.0001, help='end learning rate') 
 parser.add_argument('--seed', default=False, action='store_true', help='fix the initialization of parameters')
 parser.add_argument('--tensorboard', default=False, action='store_true', help=' use tensorboard for visualization') 
-parser.add_argument('--data_path', type=str, default="/data/haowen_yu/code/dataset/flowers")
+parser.add_argument('--use_amp', default=False, action='store_true', help=' training with mixed precision') 
+parser.add_argument('--data_path', type=str, default="/mnt/d/Datasets/flower")
 parser.add_argument('--model', type=str, default="vgg", help=' select a model for training') 
 parser.add_argument('--device', default='cuda', help='device id (i.e. 0 or 0,1 or cpu)')
 
@@ -69,7 +70,7 @@ def main(args):
 
     if opt.tensorboard:
         # 这是存放你要使用tensorboard显示的数据的绝对路径
-        log_path = os.path.join('/data/haowen_yu/code/classification/results/tensorboard' , args.model)
+        log_path = os.path.join('./results/tensorboard' , args.model)
         print('Start Tensorboard with "tensorboard --logdir={}"'.format(log_path)) 
 
         if os.path.exists(log_path) is False:
@@ -91,9 +92,7 @@ def main(args):
                                    transforms.ToTensor(),
                                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])} 
  
-    # 对标pytorch封装好的ImageFlolder，我们自己实现了一个数据加载类 Five_Flowers_Load，并使用指定的预处理操作来处理图像，结果会同时返回图像和对应的标签。 
-    # train_dataset = datasets.ImageFolder(root=os.path.join(data_path, "train"), transform=data_transform["train"])
-    # validate_dataset = datasets.ImageFolder(root=os.path.join(data_path, "val"), transform=data_transform["val"])
+    # 对标pytorch封装好的ImageFlolder，我们自己实现了一个数据加载类 Five_Flowers_Load，并使用指定的预处理操作来处理图像，结果会同时返回图像和对应的标签。  
     train_dataset = Five_Flowers_Load(os.path.join(args.data_path , 'train'), transform=data_transform["train"])
     val_dataset = Five_Flowers_Load(os.path.join(args.data_path , 'val'), transform=data_transform["val"]) 
  
@@ -110,14 +109,14 @@ def main(args):
     # create model
     model = classic_models.find_model_using_name(opt.model, num_classes=opt.num_classes).to(device) 
 
-    pg = [p for p in model.parameters() if p.requires_grad]
-    # optimizer = optim.SGD(pg, lr=args.lr, momentum=0.9, weight_decay=4E-5)
+    pg = [p for p in model.parameters() if p.requires_grad] 
     optimizer = optim.Adam(pg, lr=args.lr)
 
     # Scheduler https://arxiv.org/pdf/1812.01187.pdf
     lf = lambda x: ((1 + math.cos(x * math.pi / args.epochs)) / 2) * (1 - args.lrf) + args.lrf  # cosine
     scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lf)
     best_acc = 0.
+    
     # save parameters path
     save_path = os.path.join(os.getcwd(), 'results/weights', args.model)
     if os.path.exists(save_path) is False:
@@ -125,9 +124,8 @@ def main(args):
 
     for epoch in range(args.epochs):
         # train
-        mean_loss, train_acc = train_one_epoch(model=model, optimizer=optimizer, data_loader=train_loader, device=device, epoch=epoch, lr_method=warmup)
+        mean_loss, train_acc = train_one_epoch(model=model, optimizer=optimizer, data_loader=train_loader, device=device, epoch=epoch, use_amp=args.use_amp, lr_method= warmup)
         scheduler.step()
-
         # validate
         val_acc = evaluate(model=model, data_loader=val_loader, device=device)
 
@@ -148,7 +146,5 @@ def main(args):
             best_acc = val_acc
             torch.save(model.state_dict(), os.path.join(save_path, "AlexNet.pth")) 
 
-if __name__ == '__main__':
-
-
-    main(opt)
+        
+main(opt)
