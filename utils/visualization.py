29e7bc2ba348bcb77a8e722bfae6f05582a13920
Arwin-Yu <102544244+Arwin-Yu@users.noreply.github.com>
2023-02-12 07:57:42 +0000

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -7,43 +7,52 @@ from PIL import Image
 from torchvision import transforms
 
 class AlexNet(nn.Module):
-    def __init__(self, num_classes=1000, init_weights=False):
-        super(AlexNet, self).__init__()
+    def __init__(self, num_classes=5, init_weights=False):
+        super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 48, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[48, 55, 55]
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),  # input[3, 224, 224]  output[96, 55, 55]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[48, 27, 27]
-            nn.Conv2d(48, 128, kernel_size=5, padding=2),           # output[128, 27, 27]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[96, 27, 27]
+
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),           # output[256, 27, 27]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 13, 13]
-            nn.Conv2d(128, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[256, 13, 13]
+
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),          # output[384, 13, 13]
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 192, kernel_size=3, padding=1),          # output[192, 13, 13]
+
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),          # output[384, 13, 13]
             nn.ReLU(inplace=True),
-            nn.Conv2d(192, 128, kernel_size=3, padding=1),          # output[128, 13, 13]
+
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),          # output[256, 13, 13]
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[128, 6, 6]
+            nn.MaxPool2d(kernel_size=3, stride=2),                  # output[256, 6, 6]
         )
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5),
-            nn.Linear(128 * 6 * 6, 2048),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
+
             nn.Dropout(p=0.5),
-            nn.Linear(2048, 2048),
+            nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(2048, num_classes),
+            
+            nn.Linear(4096, num_classes),
         )
+
         if init_weights:
             self._initialize_weights()
-
+            
+ 
     def forward(self, x):
         outputs = []
         for name, module in self.features.named_children():
             x = module(x)
-            if name in ["0", "3", "6"]:
+            if name in ["0"]:
                 outputs.append(x)
 
         return outputs
+
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -56,7 +65,6 @@ class AlexNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
 
-
 data_transform = transforms.Compose(
     [transforms.Resize((224, 224)),
      transforms.ToTensor(),
@@ -65,12 +73,12 @@ data_transform = transforms.Compose(
 # create model
 model = AlexNet(num_classes=5) 
 # load model weights
-model_weight_path = "./AlexNet.pth"   
+model_weight_path = "../results/weights/alexnet/AlexNet.pth"   
 model.load_state_dict(torch.load(model_weight_path))
 print(model)
 
 # load image
-img = Image.open("../tulip.jpg")
+img = Image.open("./sunflower.jpg")
 # [N, C, H, W]
 img = data_transform(img)
 # expand batch dimension
@@ -91,3 +99,4 @@ for feature_map in out_put:
         # [H, W, C]
         plt.imshow(im[:, :, i], cmap='gray')
     plt.show()
+    plt.savefig("./AelxNet_vis.jpg")
